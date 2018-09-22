de0be82711ddd65f3db36b04edcb0d800c527853
Felix A. Guzman <felixalejandro@outlook.com>
2018-09-22 11:12:53 -0400

    Creando proyecto

---
+++ src/main/java/com/tarea/practica10/Practica10Application.java
@@ -0,0 +1,12 @@
+package com.tarea.practica10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practica10Application.class, args);
+    }
+}
